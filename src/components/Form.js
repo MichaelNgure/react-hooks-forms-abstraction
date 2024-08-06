d5001ed698898a42e2f97fc862f29e28fb7fd5e6
MichaelNgure <mikynjoro01@gmail.com>
2024-08-06 12:13:43 +0300

    react form abstraction

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,52 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  // const [firstName, setFirstName] = useState("John");
+  // const [lastName, setLastName] = useState("Henry");
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  const [formData, setFormData] = useState({
+    firstName: 'John',
+    lastName: 'Henry'
+  })
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  // function handleFirstNameChange(event) {
+  //   // setFirstName(event.target.value);
+  //   setFormData({...formData, firstName: event.target.value})
+  // }
+
+  // function handleLastNameChange(event) {
+  //   // setLastName(event.target.value);
+  //   setFormData({...formData, lastName: event.target.value})
+  // }
+
+  function handleChange(event){
+    const name = event.target.name
+    let value = event.target.value
+
+    // use `checked` property of checkboxes instead of value 
+
+    if (event.target.type === 'checkbox'){
+      value = event.target.checked
+    }
+
+    setFormData({...formData, [name]: value})
+
+    function handleSubmit(event){
+      event.preventDefault()
+      console.log(formData)
+    }
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={handleChange} value={formData.firstName}  name="firstName"/>
+      <input type="text" onChange={handleChange} value={formData.lastName}  name="lastName"/>
+      <input 
+      type="checkbox"
+      name="admin"
+      onChange={handleChange}
+      value={formData.name}
+      />
       <button type="submit">Submit</button>
     </form>
   );
